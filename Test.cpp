220191422f9ef100d41cc812be9e1836ae7a402f
Chris Thomson <tofapost@hotmail.com>
2022-02-23 00:13:38 +1300

    added fade - incomplete

--- Test.cpp
+++ Test.cpp
@@ -24,12 +24,15 @@ led_state (the actual state of the output pin, on, off, pwm value)
 
 //outputs pins---------------------------------------------------------
 const int led_green_outPin = 5;   //green led is connectd to this Pin
+const int led_red_outPin = 6;   //green led is connectd to this Pin
 
 void loop() {
-{
-    int led_green_state = led_mode_f (led_on);
-    digitalWrite(led_green_outPin, led_green_state);
-  }
+
+  int led_green_state = led_mode_f (led_on);
+  digitalWrite(led_green_outPin, led_green_state);
+
+  int led_red_state = led_mode_f (led_blink);
+  digitalWrite(led_red_outPin, led_red_state);
 
 }
 
@@ -70,8 +73,8 @@ int led_mode_f(led_mode_t led_mode) { //name of the function and what is being p
 
 
 //blink timer function
-unsigned long blink_timer_millis; //last time flasher changed on/off state
-int blink_interval = 750; // on/off time for indicator flasher (1 to 2 Hz is acceptable)
+unsigned long blink_timer_millis; //last time blink changed on/off state
+int blink_interval = 750; // on/off time for indicator blink 
 //
 int get_blink_state_f(void) { //name of the function and what is being passed in, in this case nothing (void)
   int blink_state; //what will be retunred
@@ -88,4 +91,40 @@ int get_blink_state_f(void) { //name of the function and what is being passed in
   }
 
 return blink_state;
+}
+
+
+
+/* in complete 
+
+//fade timer function
+unsigned long fade_timer_millis; //last time fade changed on/off state
+int fade_interval = 750; // on/off time for fade
+int fade_steps = 10; // number of steps in the fade
+int fade_step_millis = fade_interval / fade_steps; // the time between each step change
+int fade_step_brightness = 255 / fade_steps; // The change in brightness at each step
+int fade_direction = up // fade direction up or down
+//
+int get_fade_state_f(void) { //name of the function and what is being passed in, in this case nothing (void)
+  int fade_state; //what will be retunred
+  
+  if (millis() - fade_timer_millis >= fade_step_millis) {
+    // this is checking if the time past is higher than or equil to the step specified
+    fade_timer_millis = millis(); // re sets the timers starting value
+    // slowly increase the decrese the brightness 0-255:
+    if (fade_state >= 0) {
+      fade_state = fade_state + fade_step_brightness;
+    } 
+    if (fade_state >= 255) {
+      fade_state = fade_state - fade_step_brightness;
+    }
+    if (fade_state = 255) {
+      fade_state = fade_state - fade_step_brightness;
+    }
+  }
+
+return blink_state;
+
+*/ 
+
 }
