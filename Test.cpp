d349476ffa5a4c9168dff5a855a32596a8d685dd
Chris Thomson <tofapost@hotmail.com>
2022-02-22 23:21:02 +1300

    updeded to LED. function

--- Test.cpp
+++ Test.cpp
@@ -1,154 +1,91 @@
-/* This code is used to drive a 12V automotie IO box which takes user input from switches on handle bars or steering wheel and translates these to high power outputs.
-    The inputs are all 20V tolarnt, with an drain to GND trigger on the input pin.
-    The outputs will be switched via MOSFEST rated for a maximum of 20V, 10A for all lights, 30A for starter.
-    The IO box will also has various safetly feature to prevent damage.
-    - Short ciruit protection on all outputs
-    - Over current protection on all outputs
-    - 20V tolarant inputs
-    For expansion, the IO box also has a LIN bus port this will be wired into the MCU vial serial TX/RX
-    Curent sensing on all outputs to check for blow bulbs.
-    Progamabe input butttons to change from a swithc to a push button fuction.
+  //led function with multiple output options
 
-    LED
+/*
+
+In my main loop I would like to be able to use something like the following and aplly different modes to any led pin
+led pin to be changed = led mode it is set to
+led_x = led_off
+led_x = led_on
+led_x = led_blink
+led_x = led_fade
+led_x = led_fade_on
+led_x = led_fade_off
+
+Data to be passed into the function :
+led_mode
+
+Data to come back out of the function :
+led_state (the actual state of the output pin, on, off, pwm value)
+
+
 */
-// Global variables
-int flasher_state;
-unsigned long flasher_short_millis;
-int flasher_short_interval = 3000;
 
-
-// Include these functions
-int check_button_f(int inPin, int in_type); //returns in_state
-int get_flashing_state(void);
-
-
-//input pins----------------------------------------------------------
-const int left_inPin = 2;   //left turn switch is connectd to this Pin
-const int right_inPin = 3;  //right turn switch is connectd to this Pin
-
+//main loop example
 
 //outputs pins---------------------------------------------------------
-const int left_outPin = 5;   //left turn light is connectd to this Pin
-const int right_outPin = 6;  //right turn light is connectd this Pin
+const int led_green_outPin = 5;   //green led is connectd to this Pin
 
+void loop() {
+{
+    int led_green_state = led_mode_f (led_on);
+    digitalWrite(led_green_outPin, led_green_state);
+  }
 
-//input type---------------------------------------------------
-typedef enum {  //various types of inputs, buttons etc
-  button_latching = 0,
-  button_momentary,
-  anolog,
-} input_type_t;
-
-input_type_t left_in_type = button_momentary;
-input_type_t right_in_type = button_momentary;
-
-
-//input states---------------------------------------------------
-typedef enum {  //various states that an input can be in
-  input_off = 0,
-  input_on,
-  input_short,
-  input_long,
-  input_value,
-} input_state_t;
-
-input_state_t left_in_state = input_off;
-input_state_t right_in_state = input_off;
-input_state_t in_state = input_off;
-
-
-//outputs states---------------------------------------------------------
-// stores the state of the output pins
-typedef enum {          //various states that an output can be in      //
-  output_off = 0,       //
-  output_on,        // typedef enum (Type define enumeration) creates a new type of variable that has the states listed avalible
-  output_flashing_long,  //continues to flash only stoping when button is pressed
-  output_flashing_short, //only flashes for a set amount of time eg. 3 seconds
-                        //
-} output_state_t;       //
-
-output_state_t left_out_state = output_off;
-output_state_t right_out_state = output_off;
-
-
-//_____________________________________________________________________________________
-void setup() {  // setup code runs once
-  //_____________________________________________________________________________________
-
-  //inputs, setup pins to input---------------------------------------------------------
-  pinMode(left_inPin, INPUT_PULLUP);
-  pinMode(right_inPin, INPUT_PULLUP);
-
-  //outputs, setup pins to output---------------------------------------------------------
-  pinMode(left_outPin, OUTPUT);
-  pinMode(right_outPin, OUTPUT);
-
-  //output defualt state at start up
-  digitalWrite(left_outPin, LOW);
-  digitalWrite(right_outPin, LOW);
 }
 
-//_____________________________________________________________________________________
-void loop() {
-  //_____________________________________________________________________________________
+//led modes---------------------------------------------------
+typedef enum {  //various modes that the led can be in
+  led_off = 0,
+  led_on,
+  led_blink,
+  led_fade,
+  led_fade_on,
+  led_fade_off,
+} led_mode_t;
 
-  flasher_state = get_flashing_state_f();
 
-  // ----------------left indicator ----------------
-  left_in_state = check_button_short_long_f (left_inPin, left_in_type);  //read HW pin with Debounce //returns in_state
+//led mode function
+int led_mode_f(led_mode_t led_mode) { //name of the function and what is being passed in, in this case the led mode
+  int led_state;
 
-  // Short flash - turn on the indicator for a short period
-  if (left_in_state == input_short && left_out_state == output_off) {
-        flasher_short_millis = millis();
-        left_out_state = output_flashing_short; // this line just updates what state the output is in to determin what change is needed to when a press is detected. 
-       digitalWrite(left_outPin, flasher_state);
+  // code to determin led state  
+
+  if (led_mode == led_off) {
+    led_state = 0;
   }
-    
-  // turn off indicator if it has been flashing for long enough. 
-  if (left_out_state == output_flashing_short && millis() - flasher_short_millis > flasher_short_interval ) {
-        left_out_state = output_off;
-       digitalWrite(left_outPin, output_off);
-  }
-
-  // turn on the indicator perminently with long button press
-  if (left_in_state == input_long) {
-    left_out_state = output_flashing_long;
-    digitalWrite(left_outPin, flasher_state);
-  }
-
-  // turn off the indicator with button press
-  if (left_in_state == input_short && left_out_state != output_off) {
-    left_out_state = output_off;
-    digitalWrite(left_outPin, output_off);
-  }
-
+  return led_state;
   
-  // ----------------right indicator ----------------
-  right_in_state = check_button_short_long_f (right_inPin, right_in_type);  //read HW pin with Debounce //returns in_state
+   if (led_mode == led_on) {
+    led_state = 1;
+  }
+  return led_state;
 
-  // turn on the indicator for a short period
-  if (right_in_state == input_short && right_out_state == output_off) {
-        flasher_short_millis = millis();
-        right_out_state = output_flashing_short; // this line just updates what state the output is in to determin what change is needed to when a press is detected. 
-       digitalWrite(right_outPin, flasher_state);
+   if (led_mode == led_blink) {
+    led_state = get_blink_state_f();
+  }
+  return led_state;
+
+}
+
+
+
+//blink timer function
+unsigned long blink_timer_millis; //last time flasher changed on/off state
+int blink_interval = 750; // on/off time for indicator flasher (1 to 2 Hz is acceptable)
+//
+int get_blink_state_f(void) { //name of the function and what is being passed in, in this case nothing (void)
+  int blink_state; //what will be retunred
+  
+  if (millis() - blink_timer_millis >= blink_interval) {
+    // this is checking if the time past is higher than or equil to the interval specified
+    blink_timer_millis = millis(); // re sets the timers starting value
+    // if the light is off turn it on and vice-versa:
+    if (blink_state == 0) {
+      blink_state = 1;
+    } else {
+      blink_state = 0;
+    }
   }
 
-  // turn off indicator if it has been flashing for long enough. 
-  if (right_out_state == output_flashing_short && millis() - flasher_short_millis > flasher_short_interval ) {
-        right_out_state = output_off;
-       digitalWrite(right_outPin, output_off);
-  }
-
-  // turn on the indicator perminently with long button press
-  if (right_in_state == input_long) {
-    right_out_state = output_flashing_long;
-    digitalWrite(right_outPin, flasher_state);
-  }
-
-  // turn off the indicator with button press
-  if (right_in_state == input_short && right_out_state != output_off) {
-    right_out_state = output_off;
-    digitalWrite(right_outPin, output_off);
-  }
-
+return blink_state;
 }
