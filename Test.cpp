b99cfc3cca2fc99840076ded7d3948e1fdc8a964
Chris Thomson <tofapost@hotmail.com>
2022-02-23 13:51:57 +1300

    update

--- Test.cpp
+++ Test.cpp
@@ -26,6 +26,7 @@ led_state (the actual state of the output pin, on, off, pwm value)
 const int led_green_outPin = 5;   //green led is connectd to this Pin
 const int led_red_outPin = 6;   //green led is connectd to this Pin
 
+
 void loop() {
 
   int led_green_state = led_mode_f (led_on);
@@ -74,10 +75,11 @@ int led_mode_f(led_mode_t led_mode) { //name of the function and what is being p
 
 //blink timer function
 unsigned long blink_timer_millis; //last time blink changed on/off state
-int blink_interval = 750; // on/off time for indicator blink 
+int blink_interval = 750; // on/off time for indicator blink
+int blink_state; //what will be retunred
 //
 int get_blink_state_f(void) { //name of the function and what is being passed in, in this case nothing (void)
-  int blink_state; //what will be retunred
+
   
   if (millis() - blink_timer_millis >= blink_interval) {
     // this is checking if the time past is higher than or equil to the interval specified
@@ -97,14 +99,15 @@ return blink_state;
 
 //fade timer function
 unsigned long fade_timer_millis; //last time fade changed on/off state
+int fade_state; //what will be retunred
 int fade_interval = 750; // on/off time for fade
 int fade_steps = 10; // number of steps in the fade
 int fade_step_millis = fade_interval / fade_steps; // the time between each step change
 int fade_step_brightness = 255 / fade_steps; // The change in brightness at each step
-bool fade_up = true // fade direction up or down
+bool fade_up = true; // fade direction up or down
 //
 int get_fade_state_f(void) { //name of the function and what is being passed in, in this case nothing (void)
-  int fade_state; //what will be retunred
+
   
   if (millis() - fade_timer_millis >= fade_step_millis) {
     // this is checking if the time past is higher than or equil to the step specified
@@ -114,14 +117,14 @@ int get_fade_state_f(void) { //name of the function and what is being passed in,
       if (fade_state <= 255) {
       fade_state = fade_state + fade_step_brightness;
     }
-    else (fade_up = false)
+    else fade_up = false;
     }
 
     if (fade_up == false){
       if (fade_state >= 0) {
       fade_state = fade_state - fade_step_brightness;
     }
-    else (fade_up = true)
+    else fade_up = true;
     }
 
   }
