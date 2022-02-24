af4a9bb296c55b22a280a91162a5019b2b703921
Chris Thomson <tofapost@hotmail.com>
2022-02-24 17:01:09 +1300

    fixed unsigned signed warning

--- src/led_set_mode_example_use.cpp
+++ src/led_set_mode_example_use.cpp
@@ -7,20 +7,23 @@ void setup() {
   // put your setup code here, to run once:
 pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
 
 }
 
 //outputs pins---------------------------------------------------------
 const int led_green_outPin = 5;   //green led is connectd to this Pin
-const int led_red_outPin = 6;   //green led is connectd to this Pin
+const int led_red_outPin = 6;   //red led is connectd to this Pin
+const int led_blue_outPin = 7;   //blue led is connectd to this Pin
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-//main loop example
+//main loop example to run repeatedly:
 
   analogWrite(led_green_outPin, led_set_mode_f(led_on));
 
   analogWrite(led_red_outPin, led_set_mode_f(led_blink));
+
+  analogWrite(led_blue_outPin, led_set_mode_f(led_glow));
 
 }
